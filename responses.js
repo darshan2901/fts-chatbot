f5c7be095c9b953ecad1bb6b0d5d873864a5f9e4
Darshan Patel <darshanthumar2901@gmail.com>
2021-06-29 15:03:04 +0530

    response.js updated

--- responses.js
+++ responses.js
@@ -1,28 +1,30 @@
 function getBotResponse(input) {
     //rock paper scissors
+    if (input == "rock") {
+        return "paper";
+    } else if (input == "paper") {
+        return "scissors";
+    } else if (input == "scissors") {
+        return "rock";
+    }
+
+  
+
+    // Simple responses
     if (input == "hey") {
         return "How can I help you";
     } else if (input == "hello") {
         return "How can I help you";
     } else if (input == "good Morning") {
-        return "rHow can I help you";
-    }
-
-    // Simple responses
-    if (input == "hello") {
-        return "Hello there!";
-    } else if (input == "hi") {
-        return "Hello!";
+        return "How can I help you";
     }else if (input == "goodbye") {
         return "Talk to you later!";
-    }else if (input == "goodbye") {
-        return "Talk to you later!";
-    }else if (input == "goodbye") {
-        return "Talk to you later!";
-    }else if (input == "goodbye") {
-        return "Talk to you later!";
-    }else if (input == "goodbye") {
-        return "Talk to you later!";
+    }else if (input == "what's up") {
+        return "I'm Fine";
+    }else if (input == "See you soon") {
+        return "Take care";
+    }else if (input == "bored") {
+        return "Same here!";
     }else {
         return "Try asking something else!";
     }
